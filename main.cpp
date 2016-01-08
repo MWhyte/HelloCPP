938a22fd413e90122a23013156167381c7238e88
Michael Whyte <mrmwhyte@gmail.com>
2016-01-08 20:49:55 +0000

    IO

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-
 using namespace std;
 
-
-int num = 34;               // 4 bytes allocated
-string name = "John";       // variable size
-double price = 12.1;        // 8 bytes allocated
-char character = 'C';       // 1 bytes allocated
-bool trueOrFalse = true;    // 1 bytes allocated
-
 int main() {
-    cout << num << endl;
-    cout << name << endl;
-    cout << price << endl;
-    cout << character << endl;
-    cout << trueOrFalse << endl;
+    string name = "";
+    int age = 0;
+
+    cout << "Enter you name:" << endl;
+    cin >> name;
+
+    cout << "Enter you age:" << endl;
+    cin >> age;
+
+
+    cout << "Hello " << name << " you are " << age << " years old." << endl;
+
     return 0;
 }
