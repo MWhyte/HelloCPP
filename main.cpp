c30f3f32a9bc9121039fe11848b5b29348c5900f
Michael Whyte <mrmwhyte@gmail.com>
2016-01-08 22:31:41 +0000

    IO

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    string name = "";
-    int age = 0;
 
-    cout << "Enter you name:" << endl;
-    cin >> name;
-
-    cout << "Enter you age:" << endl;
-    cin >> age;
+    // Get the name of the movie
+    string movie = "";
+    cout << "What is the name of the movie:" << endl;
+    getline(cin, movie);
 
 
-    cout << "Hello " << name << " you are " << age << " years old." << endl;
+    // Get Metascore
+    int metascore = 0;
+    cout << "What is the metascore rating for " << movie << "?" << endl;
+    cin >> metascore;
+
+    // Get Tomatometer score
+    int tomatometer = 0;
+    cout << "What is the Tomatometer rating for " << movie << "?" << endl;
+    cin >> tomatometer;
+
+
+
+    // calculate average
+    int averageScore = (metascore + tomatometer) / 2;
+
+    cout << "Average score is " << averageScore << endl;
 
     return 0;
 }
