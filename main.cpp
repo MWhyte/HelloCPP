8ad529250187a5c66311cc04a895f4767564a847
Michael Whyte <mrmwhyte@gmail.com>
2016-01-08 20:41:22 +0000

    variables

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>                         // include directive. Make other files availble to us.
-                                            // iostream is a standard library for IO
+#include <iostream>
 
-using namespace std;                        // similar to a static import in java
 
-int main() {                                // standard signature for a main function
-    cout << "Hello, World!" << endl;        // cout = character output stream, << insertion operator, endl = \n
-    return 0;                               // return type  = exit code
+using namespace std;
+
+
+int num = 34;               // 4 bytes allocated
+string name = "John";       // variable size
+double price = 12.1;        // 8 bytes allocated
+char character = 'C';       // 1 bytes allocated
+bool trueOrFalse = true;    // 1 bytes allocated
+
+int main() {
+    cout << num << endl;
+    cout << name << endl;
+    cout << price << endl;
+    cout << character << endl;
+    cout << trueOrFalse << endl;
+    return 0;
 }
