8fad44f856e34b7e74dd0341085fa5845e25b315
Michael Whyte <mrmwhyte@gmail.com>
2016-01-09 19:55:31 +0000

    loops

--- main.cpp
+++ main.cpp
@@ -4,76 +4,27 @@ using namespace std;
 
 int main() {
 
-    int x = 10;
-    int y = 20;
+    int x = 0;
 
-
-    // if
-    if (x == 10){
-        cout << "X equals ten" << endl;
+    // while loop
+    while (x < 2){
+        cout << x << endl;
+        x++;
     }
 
-    // if else
-    if (x == 10){
-        cout << "X equals ten" << endl;
-    } else {
-        cout << "X does not equal ten" << endl;
+    // do while
+    x = 0;
+
+    do{
+        cout << x << endl;
+        x++;
+    } while (x < 2);                // executes once before evaluating this expression
+
+
+    // for loop
+    for (int i = 0; i < 10; i++){
+        cout << i << endl;
     }
-
-
-    // nested if
-    if (x == 10){
-        if (y == 20){
-            cout << "X equals ten and y equals 20" << endl;
-        } else {
-            cout << "X equals ten and y does not equal 20" << endl;
-        }
-    } else {
-        if (y == 20){
-            cout << "X does not equal ten and y equals 20" << endl;
-        } else {
-            cout << "X does not equal ten and y doesn not equal 20" << endl;
-        }
-    }
-
-
-    // operators and no brackets
-    if (x==10 && y==20)
-        cout << "X equals ten and y equals 20" << endl;
-
-
-    if (x==10 || y==20)
-        cout << "X equals ten OR y equals 20" << endl;
-
-
-
-    // switch statement
-    char grade = 'D';
-
-    switch(grade)
-    {
-        case 'A' :
-            cout << "Excellent!" << endl;
-            break;
-        case 'B' :
-        case 'C' :
-            cout << "Well done" << endl;
-            break;
-        case 'D' :
-            cout << "You passed" << endl;
-            break;
-        case 'F' :
-            cout << "Better try again" << endl;
-            break;
-        default :
-            cout << "Invalid grade" << endl;
-    }
-    cout << "Your grade is " << grade << endl;
-
-    return 0;
-
-
-
 
     return 0;
 }
