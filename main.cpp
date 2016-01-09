907a0d51ecbcf915564b12354ff6f72dd442f5f1
Michael Whyte <mrmwhyte@gmail.com>
2016-01-09 18:32:16 +0000

    conditional operators - switch statements

--- main.cpp
+++ main.cpp
@@ -46,5 +46,34 @@ int main() {
         cout << "X equals ten OR y equals 20" << endl;
 
 
+
+    // switch statement
+    char grade = 'D';
+
+    switch(grade)
+    {
+        case 'A' :
+            cout << "Excellent!" << endl;
+            break;
+        case 'B' :
+        case 'C' :
+            cout << "Well done" << endl;
+            break;
+        case 'D' :
+            cout << "You passed" << endl;
+            break;
+        case 'F' :
+            cout << "Better try again" << endl;
+            break;
+        default :
+            cout << "Invalid grade" << endl;
+    }
+    cout << "Your grade is " << grade << endl;
+
+    return 0;
+
+
+
+
     return 0;
 }
